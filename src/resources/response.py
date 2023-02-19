eaacf936924c43a52e2e04c86aa4736f2cd1eb28
Lorttexwolf <comisgammer@gmail.com>
2023-02-19 00:29:00 -0600

    expanded response options

--- src/resources/response.py
+++ src/resources/response.py
@@ -1,5 +1,5 @@
 import hikari
-
+from .emojis import LOADING_EMOJI
 
 
 class Response:
@@ -24,3 +24,32 @@ class Response:
                 hikari.ResponseType.MESSAGE_CREATE,
                 content, embed=embed, component=components, **kwargs
             )
+            
+    async def edit(
+        self,
+        content: str = None,
+        embed: hikari.Embed = None,
+        components: list = None,
+        **kwargs):
+
+        if self.responded or self.deferred:
+            await self.interaction.edit_initial_response(content, embed=embed, component=components, **kwargs)
+        else:
+            raise RuntimeError("Cannot edit initial message before one has been sent.")
+        
+    async def edit_or_send(self,
+        content: str = None,
+        embed: hikari.Embed = None,
+        components: list = None,
+        **kwargs):
+        '''Helper function to edit or send a message depending if one has already been sent.'''
+        
+        if self.responded or self.deferred:
+            await self.edit(content, embed, components, **kwargs)
+        else:
+            await self.send(content, embed, components, **kwargs)
+
+    async def loading(self, content: str):
+        '''Responds to the message with a seemingly deferred loading message to the user.'''
+        await self.edit_or_send(embed = hikari.Embed(description=f"{LOADING_EMOJI}\t  {content}", color="#2f3136"))
+
