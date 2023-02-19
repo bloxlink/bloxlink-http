ad0f81ca7540731001640e202c8342bb32476491
Lorttexwolf <comisgammer@gmail.com>
2023-02-19 00:23:11 -0600

    add emoji resource

---
+++ src/resources/emojis.py
@@ -0,0 +1,5 @@
+from hikari import KnownCustomEmoji
+
+# TODO: Make these configurable.
+LOADING_EMOJI = KnownCustomEmoji.parse("<a:loading:1072621230500352031>")
+ROBLOX_SIMPLE_EMOJI = KnownCustomEmoji.parse("<:roblox:1070141053040066640>")
